b76677f8415e43f94f3ae2d7c89d0f0e1516992f
soumyajit <soumyajitkar47@gmail.com>
2025-11-08 08:16:40 +0530

    New commit of SPOTIFY project on the 08-11-2025

--- spotify.js
+++ spotify.js
@@ -1,1 +1,54 @@
-console.log(lets)
+console.log('Lets write some code with the java Script')
+
+
+async function main() {
+    let a =await fetch("http://127.0.0.1:3000/songs/")
+    let response=await a.text();
+    console.log(response)
+    let div=document.createElement("div")
+    div.innerHTML=response;
+    div.getElementByTagName("a")
+     console.log(TextDecoderStream)
+let songs=[]
+    for(let index=0;index<as.length;index++){
+        const element=as[index];
+        if(element.href.endWith(".mp3")){
+              songs.push(element.href)
+
+
+        }
+
+    }
+return songs
+
+}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
